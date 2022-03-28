e6ab0610388d6077ffc6858d17c0f23f88c68c87
brisney quiñonez <brisneyducuara@gmail.com>
2022-03-28 04:42:36 -0500

    formulario de registro con estilos

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CocineroComponent } from './views/cocinero/cocinero.component';
 import { HomeComponent } from './views/home/home.component';
 import { MeseroComponent } from './views/mesero/mesero.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {path: '**', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'mesero', component: MeseroComponent},
   {path: 'cocinero', component: CocineroComponent},
+  {path: 'registro', component: RegisterComponent}
 ];
 
 @NgModule({
